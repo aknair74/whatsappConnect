9e062d45d9a587066dfe6f1f7e3b8089364b080b
aknair74 <70668207+aknair74@users.noreply.github.com>
2020-09-02 15:18:24 -0700

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,15 +1,13 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
 account_sid = 'AC78087c23ae1dc687ff7ca9bed5f0f74e'
-auth_token = 'token'
+auth_token = '2f840caa5f8b9ec9fde0caed6f2d0d8a'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Hello there from !Twilio!',
+                              body='Hello there from Twilio!',
                               from_='whatsapp:+14155238886',
                               to='whatsapp:+19256395097'
                           )
