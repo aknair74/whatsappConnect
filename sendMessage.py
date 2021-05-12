50b33bb392cbcc741d2ca605d55242ce4ac06773
aknair74 <70668207+aknair74@users.noreply.github.com>
2021-05-12 11:23:49 -0700

    Update sendMessage.py

--- sendMessage.py
+++ sendMessage.py
@@ -2,6 +2,7 @@ from twilio.rest import Client
 #from credentials import account_sid, auth_token, my_cell, my_twilio
 account_sid = 'AC78087c23ae1dc687ff7ca9bed5f0f74e'
 auth_token = 'your_token'
+#Test
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
